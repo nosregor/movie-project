488447d1b8089c743fe9b58a36259f6e95dd39c2
Andrew Rogerson <werdnanosregor@gmail.com>
2018-11-09 10:53:19 +0100

    Done in class

---
+++ models/Movie.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const movieSchema = new Schema({
+  title: String,
+  description: String,
+  imgName: String,
+  imgPath: String,
+}, {
+  timestamps: { createdAt: 'createdAt', updatedAt: 'updatedAt' },
+});
+
+const Movie = mongoose.model('Movie', movieSchema);
+
+module.exports = Movie;
---
+++ routes/index.js
@@ -0,0 +1,42 @@
+const express = require('express');
+const Movie = require('../models/movie.js');
+const uploadCloud = require('../config/cloudinary.js');
+
+const router = express.Router();
+
+/* GET home page */
+router.get('/', (req, res, next) => {
+  Movie.find()
+    .then((movies) => {
+      res.render('index', { movies });
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+router.get('/movie/add', (req, res, next) => {
+  res.render('movie-add');
+});
+
+//  uploadCloud.single('photo') is a middleware
+// the parameter is 'photo' because we have
+
+router.post('/movie/add', uploadCloud.single('photo'), (req, res, next) => {
+  console.log(req);
+  const { title, description } = req.body;
+  const imgPath = req.file.url;
+  const imgName = req.file.originalname;
+  const newMovie = new Movie({
+    title, description, imgPath, imgName,
+  });
+  newMovie.save()
+    .then((movie) => {
+      res.redirect('/');
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+
+module.exports = router;
